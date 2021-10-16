7706f6bba926a1da6fac919e7b6142368fcd39c3
Kevin Chu <kvnchu2@gmail.com>
2021-10-16 16:58:43 -0700

    remove meta description

---
+++ precache-manifest.36201a1566342142a95bc87c8e63780d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc139b8c11a0d3802163c64c607451a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9c25fe6629028ea202e",
+    "url": "/static/css/main.624cab41.chunk.css"
+  },
+  {
+    "revision": "90077139914aea7e861a",
+    "url": "/static/js/2.b13346b8.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.b13346b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9c25fe6629028ea202e",
+    "url": "/static/js/main.c75d7e34.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "e67032ae2d488a737c31123366bf6231",
+    "url": "/static/media/kevin-bio.e67032ae.e67032ae.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
